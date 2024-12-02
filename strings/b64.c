84ceb633cf1c363da406385e2cde175c8e37c686
ohadk123 <ohad43@gmail.com>
2024-12-02 18:22:41 +0200

    cont.

--- strings/b64.c
+++ strings/b64.c
@@ -1,10 +1,19 @@
 #include <immintrin.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 #include "libstr.h"
 
-int b64_distance(char str1[MAX_STR], char str2[MAX_STR]) {
+int clear_string() {
 	char ranges[] = {'A', 'Z', 'a', 'z', '0', '9', '+','+', '/', '/', 0, 0, 0, 0, 0, 0};
 	__m128i b64_filter = _mm_lddqu_si128((const __m128i *) ranges);
-	
+	char str[] = "123456789?bcdef";
+	char dst[16] = {0};
+	__m128i xstr = _mm_lddqu_si128((const __m128i *) str);
+	__m128i mask = _mm_cmpistrm(b64_filter, xstr, _SIDD_UBYTE_OPS | _SIDD_CMP_RANGES | _SIDD_UNIT_MASK | _SIDD_MASKED_NEGATIVE_POLARITY);
+	_mm_maskmoveu_si128(xstr, mask, dst);
+	puts(dst);
+}
+
+int main() {
+	clear_string();
+	return 0;
 }
