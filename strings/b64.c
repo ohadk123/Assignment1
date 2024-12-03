52944843ed933a3999b2c01678c329701a294900
ohadk123 <ohad43@gmail.com>
2024-12-03 16:57:04 +0200

    cont b64

--- strings/b64.c
+++ strings/b64.c
@@ -1,19 +1,34 @@
 #include <immintrin.h>
 #include <stdio.h>
+#include <string.h>
 #include "libstr.h"
 
-int clear_string() {
+// extern int str_len(char *str);
+
+int clear_string(char *str, char *dst, int len) {
 	char ranges[] = {'A', 'Z', 'a', 'z', '0', '9', '+','+', '/', '/', 0, 0, 0, 0, 0, 0};
 	__m128i b64_filter = _mm_lddqu_si128((const __m128i *) ranges);
-	char str[] = "123456789?bcdef";
-	char dst[16] = {0};
 	__m128i xstr = _mm_lddqu_si128((const __m128i *) str);
-	__m128i mask = _mm_cmpistrm(b64_filter, xstr, _SIDD_UBYTE_OPS | _SIDD_CMP_RANGES | _SIDD_UNIT_MASK | _SIDD_MASKED_NEGATIVE_POLARITY);
+	__m128i mask = _mm_cmpistrm(b64_filter, xstr, _SIDD_UBYTE_OPS | _SIDD_CMP_RANGES | _SIDD_UNIT_MASK);
 	_mm_maskmoveu_si128(xstr, mask, dst);
+
+	for (int w = 0, r = 0; w < len; w++) {
+		while (dst[w++]);
+		r = w;
+		while (!(dst[r]));
+		dst[w] = dst[r];
+	}
 	puts(dst);
 }
 
 int main() {
-	clear_string();
+	char str[] = "123456789?bcdef";
+	int len = strlen(str);
+	char dst[len];
+	memset((void *) dst, 0, len);
+	clear_string(str, dst, len);
+
+	puts(dst);
+
 	return 0;
 }
